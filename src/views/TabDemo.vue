910e6d5b372009c831cb7759ebaced9d46433b42
hl <3431810276@qq.com>
2023-04-21 23:13:19 +0800

    update tab

--- src/views/TabDemo.vue
+++ src/views/TabDemo.vue
@@ -1,7 +1,24 @@
 <script setup>
+import { reactive } from 'vue'
 import Tab from '@/components/base/Tab.vue'
+
+const flux = reactive({
+  tabs1: 0,
+  tabs2: 'vue',
+})
 </script>
 
 <template>
-  <Tab />
+  <Tab
+    v-model="flux.tabs1"
+    class="my-4 bg-white dark:bg-slate-800 border dark:border-slate-600 rounded-lg"
+  >
+    <div title="Vue">
+      <div>Vue Content</div>
+    </div>
+
+    <div title="React">
+      <div>React Content</div>
+    </div>
+  </Tab>
 </template>
