4c0ccf8c15c9d9bf25e7d29ab31223054601ad40
hl <3431810276@qq.com>
2023-04-28 00:13:11 +0800

    update component:radio

--- src/components/base/Collapse.vue
+++ src/components/base/Collapse.vue
@@ -29,17 +29,8 @@ function changeCollapse() {
         </svg>
       </div>
     </div>
-    <Transition
-      enter-active-class="transition duration-200 ease-out"
-      enter-from-class="translate-y-1 opacity-0"
-      enter-to-class="translate-y-0 opacity-100"
-      leave-active-class="transition duration-150 ease-in"
-      leave-from-class="translate-y-0 opacity-100 duration-200"
-      leave-to-class="translate-y-1 opacity-0"
-    >
-      <div :class="openCollapse ? 'block' : 'hidden h-0'">
-        <slot id="content" name="content" />
-      </div>
-    </Transition>
+    <div class="duration-300 ease-in-out transform translate-y-6 " :class="openCollapse ? 'block' : 'hidden h-0'">
+      <slot id="content" name="content" />
+    </div>
   </div>
 </template>
