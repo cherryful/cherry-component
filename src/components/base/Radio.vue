ba932d8240119df1fa749fbb175ea585dc8975e1
szluyu99 <370166800@qq.com>
2023-04-29 00:47:27 +0800

    update component: Radio

--- src/components/base/Radio.vue
+++ src/components/base/Radio.vue
@@ -1,103 +1,103 @@
-<!-- <script lang="ts" setup>
-import { computed } from 'vue'
+<script setup lang="ts">
 import uniqueId from 'lodash/uniqueId'
+import { computed } from 'vue'
 
-const props = defineProps<{
-  label?: string
-  value?: unknown
-  options?: string[] | Array<{ label: string; value: unknown }>
+const props = withDefaults(defineProps<{
+  modelValue?: string | number | boolean | unknown
+  type?: 'success' | 'info' | 'warning' | 'error' | 'primary' | 'secondary' | 'accent'
+  value?: string | number | boolean | unknown
   disabled?: boolean
-}>()
+  options?: Array<{
+    title: string
+    value: string
+  }>
+  bordered?: boolean
+  hidden?: boolean
+}>(), {
+  modelValue: undefined,
+  type: 'primary',
+  disabled: false,
+  value: undefined,
+  options: () => [],
+  bordered: false,
+  hidden: false,
+})
 
-const emit = defineEmits<{
-  (evt: 'update:value', val: unknown): void
+const emits = defineEmits<{
+  (evt: 'update:modelValue', val: unknown): void
   (evt: 'change', val: unknown): void
 }>()
 
 const uid = uniqueId('radio-group-')
 
-const radioGroupValue = computed({
-  get: () => props.value,
-  set: val => emit('update:value', val),
+const radioValue = computed({
+  get: () => props.modelValue,
+  set: val => emits('update:modelValue', val),
 })
+
+const checked = computed(() => props.value === radioValue.value)
+
+const themes = {
+  primary: {
+    base: 'text-primary-600 focus:ring-primary-500',
+    border: 'border-primary-500',
+  },
+  secondary: {
+    base: 'text-secondary-600 focus:ring-secondary-500',
+    border: 'border-secondary-500',
+  },
+  accent: {
+    base: 'text-accent-600 focus:ring-accent-500',
+    border: 'border-accent-500',
+  },
+  success: {
+    base: 'text-success-600 focus:ring-success-500',
+    border: 'border-success-500',
+  },
+  info: {
+    base: 'text-info-600 focus:ring-info-500',
+    border: 'border-info-500',
+  },
+  warning: {
+    base: 'text-warning-600 focus:ring-warning-500',
+    border: 'border-warning-500',
+  },
+  error: {
+    base: 'text-error-600 focus:ring-error-500',
+    border: 'border-error-500',
+  },
+}
 </script>
 
 <template>
-  <div class="radio-group">
-    <div class="text-sm mb-2 font-bold empty:hidden">
-      {{ label }}
+  <label
+    :for="uid"
+    class="text-sm font-medium leading-6 text-gray-900 inline-flex items-center"
+    :class="[
+      disabled ? 'opacity-60 cursor-not-allowed' : 'cursor-pointer',
+      {
+        'rounded border-1 p-2 transition-colors duration-150 ease-in-out': bordered,
+        'border-gray-300': bordered && !checked && !disabled,
+        [themes[type].border]: bordered && checked,
+      },
+    ]"
+  >
+    <input
+      v-bind="$attrs"
+      :id="uid"
+      v-model="radioValue"
+      type="radio"
+      :value="value"
+      :name="uid"
+      :disabled="disabled"
+      class="h-4 w-4 border-gray-300"
+      :class="[themes[type].base, {
+        'sr-only': hidden,
+      }]"
+      @change="$emit('change', radioValue)"
+    >
+    <div :class="{ 'pl-2': $slots.default && !hidden }">
+      <slot :checked="checked" />
     </div>
-
-    <div class="flex items-center h-38px space-x-4">
-      <label
-        v-for="(item, idx) in options"
-        :key="idx"
-        class="flex items-center"
-        :class="[disabled ? 'cursor-not-allowed opacity-60' : 'cursor-pointer']"
-      >
-        <div class="relative flex justify-center items-center">
-          <input
-            :id="`${uid}-${idx}`"
-            v-model="radioGroupValue"
-            v-bind="$attrs"
-            type="radio"
-            :name="uid"
-            :value="typeof item === 'object' ? item.value : item"
-            :disabled="disabled"
-            class="radio"
-            @change="emit('change', radioGroupValue)"
-          >
-
-          <div
-            class="absolute select-none w-3.5 h-3.5 text-primary-500"
-            :class="{
-              'i-mdi-circle': radioGroupValue === (typeof item === 'object' ? item.value : item),
-            }"
-          />
-        </div>
-
-        <div class="ml-2">
-          {{ typeof item === 'object' ? item.label : item }}
-        </div>
-      </label>
-    </div>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-.radio-group {
-  @apply flex flex-col;
-}
-
-.radio {
-  @apply appearance-none w-5 h-5 rounded-full bg-white border border-slate-400 dark:border-slate-600;
-  @apply focus:outline-none focus:ring-2 focus:ring-primary-400 focus:shadow-lg;
-}
-</style> -->
-<script setup lang="ts">
-const props = defineProps<{
-  label?: string
-  options?: Array<{ id: any; title: string ;value: string }>
-}>()
-
-const emit = defineEmits<{
-  (evt: 'update:value', val: unknown): void
-  (evt: 'change', val: unknown): void
-}>()
-</script>
-
-<template>
-  <div>
-    <fieldset class="mt-4">
-      <legend class="sr-only">
-        Notification method
-      </legend>
-      <div class="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-        <div v-for="(notificationMethod, index) in props.options" :key="index" class="flex items-center">
-          <input :id="notificationMethod.id" name="notification-method" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-          <label :for="notificationMethod.id" class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ notificationMethod.title }}</label>
-        </div>
-      </div>
-    </fieldset>
-  </div>
+  </label>
 </template>
--- src/views/RadioDemo.vue
+++ src/views/RadioDemo.vue
@@ -1,18 +1,85 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref } from 'vue'
+
 import Radio from '@/components/base/Radio.vue'
 
-const flux = reactive({
-  options: [
-    { id: 'email', title: 'Email' },
-    { id: 'sms', title: 'Phone (SMS)' },
-    { id: 'push', title: 'Push notification' },
-  ],
-})
+const checkValue = ref('durian')
+const onChangeVal = ref('')
+
+function onChange(val) {
+  onChangeVal.value = `onChange: ${val}`
+}
 </script>
 
 <template>
-  <div class="flex">
-    <Radio :options="flux.options" />
+  <div class="space-x-5">
+    checked: {{ checkValue }} |
+    {{ onChangeVal }}
+  </div>
+  <div class="flex gap-4">
+    <Radio v-model="checkValue" type="success" value="apple" @change="onChange">
+      <div>
+        apple
+      </div>
+    </Radio>
+    <Radio v-model="checkValue" type="info" value="banana" @change="onChange">
+      banana
+    </Radio>
+    <Radio v-model="checkValue" type="warning" value="orange" @change="onChange">
+      orange
+    </Radio>
+    <Radio v-model="checkValue" type="error" value="pear" @change="onChange">
+      pear
+    </Radio>
+    <Radio v-model="checkValue" type="primary" value="graph" @change="onChange">
+      graph
+    </Radio>
+    <Radio v-model="checkValue" type="secondary" value="mongo" @change="onChange">
+      mongo
+    </Radio>
+    <Radio v-model="checkValue" type="accent" value="watermelon" @change="onChange">
+      watermelon
+    </Radio>
+    <Radio v-model="checkValue" disabled value="durian" @change="onChange">
+      durian
+    </Radio>
+  </div>
+  <div class="flex gap-4">
+    <Radio v-model="checkValue" bordered type="success" value="apple" @change="onChange">
+      apple
+    </Radio>
+    <Radio v-model="checkValue" bordered type="info" value="banana" @change="onChange">
+      banana
+    </Radio>
+    <Radio v-model="checkValue" bordered type="warning" value="orange" @change="onChange">
+      orange
+    </Radio>
+  </div>
+  <div class="flex gap-4">
+    <Radio v-slot="{ checked }" v-model="checkValue" hidden type="success" value="apple" @change="onChange">
+      <div
+        class="w-8 h-8 rounded-full border-2 border-white bg-success-500"
+        :class=" {
+          'ring-2 ring-success-500': checked,
+        }
+        "
+      />
+    </Radio>
+    <Radio v-slot="{ checked }" v-model="checkValue" hidden type="info" value="banana" @change="onChange">
+      <div
+        class="w-8 h-8 rounded-full border-2 border-white bg-info-500"
+        :class="{
+          'ring-2 ring-info-500': checked,
+        }"
+      />
+    </Radio>
+    <Radio v-slot="{ checked }" v-model="checkValue" hidden type="warning" value="orange" @change="onChange">
+      <div
+        class="w-8 h-8 rounded-full border-2 border-white bg-warning-500"
+        :class="{
+          'ring-2 ring-warning-500': checked,
+        }"
+      />
+    </Radio>
   </div>
 </template>
